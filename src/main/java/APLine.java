78b45da137a94925e1da4e3299e60c6fdc074f1e
Calvin <calvinlu@live.com>
2024-02-16 10:59:51 -0800

    Attempt #1

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,34 @@
 public class APLine
 {
   /* your code here */
-  
+  private final int a;
+  private final int b;
+  private final int c;
+
+  public APLine(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  public double getSlope() {
+    return (double) -a / b;
+  }
+
+  public boolean isOnLine(int x, int y) {
+    return (a * x + b * y + c) == 0;
+  }
 }
+/*
+  .--.            .--.
+ ( (`\\."--``--".//`) )
+  '-.   __   __    .-'
+   /   /__\ /__\   \
+  |    \ 0/ \ 0/    |
+  \     `/   \`     /
+   `-.  /-"""-\  .-`
+     /  '.___.'  \
+     \     I     /
+      `;--'`'--;`
+jgs     '.___.'
+*/
